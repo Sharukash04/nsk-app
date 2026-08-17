d12739e555385ddd32cfbd3946993d4ce4d9f2f8
SHARUKASH <sharukash01@gmail.com>
2026-08-18 01:35:20 +0530

    Add files via upload

---
+++ .github/workflows/capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nsk.app',
+  appName: 'NSK',
+  webDir: 'dist',
+  backgroundColor: '#121216',
+  android: {
+    backgroundColor: '#121216',
+  },
+};
+
+export default config;
